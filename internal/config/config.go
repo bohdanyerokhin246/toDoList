d7464b94098336e5518ec42c0ac7c630a9e91d81
bohdan <erohinbogdan246@gmail.com>
2025-04-28 15:18:57 +0300

    Added github.com/joho/godotenv for reading variables from .dev

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,22 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func ConnectDB() *sql.DB {
 	var err error
+
+	// Load the .env file
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	connStr := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		os.Getenv("DB_HOST_APP"),
